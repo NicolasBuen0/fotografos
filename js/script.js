5b1886fbf84178d97330574c9418953f82adc597
Nicolas Bueno <nickibueno@gmail.com>
2022-06-27 22:23:13 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const btnMobile = document.querySelector("#btn-mobile");
+
+function toggleMenu(event) {
+    if (event.type == "touchstart") event.preventDefault()
+    const nav = document.querySelector("nav");
+    nav.classList.toggle("ativo")
+}
+btnMobile.addEventListener("click", toggleMenu)
+btnMobile.addEventListener("touchstart", toggleMenu)
